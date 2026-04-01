d6374c51ca6450be4a20b32222f0deb73721554d
ElisaEscola <elisabeth.martins@escola.pr.gov.br>
2026-04-01 11:15:09 -0300

    Add files via upload

--- revisao.js
+++ revisao.js
@@ -11,9 +11,16 @@ app.get('/batataFritadinha', (req, res) => {
     let precoCafe = 12;
     let total = 0;
 
-    for (let i = 0; i < 3; i++) {
+    for (i = 0; i < 2; i++) {
         total += precoCafe;
-        console.log('`Fazendo o cafezinho ${i + 1}. Total acumulado: R$ ${total}`');
+        console.log(`Fazendo o cafezinho ${i + 1}. Total acumulado: R$ ${total}`);
+
+    }
+    if (total >= 30) {
+        console.log("Desconto aplicado")
+    }
+    else  {
+        console.log("Desconto não aplicado")
     }
 
 
